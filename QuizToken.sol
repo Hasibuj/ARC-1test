07411d57b34945bdaaf6dd3f7536e1965f4c869f
Md Hasib <103415835+Hasibuj@users.noreply.github.com>
2026-05-18 11:46:06 +0600

    Update QuizToken.sol

--- QuizToken.sol
+++ QuizToken.sol
@@ -1,25 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./IERC20.sol";
-import "./Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract QuizToken is IERC20, Ownable {
     string private _name = "ArcQuiz Token";
     string private _symbol = "AQT";
     uint8 private _decimals = 18;
     uint256 private _totalSupply;
+    
+    // Setting a maximum supply limit (e.g., 10 Million Tokens)
+    uint256 public constant MAX_SUPPLY = 10000000 * 10**18;
 
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
     constructor() Ownable(msg.sender) {}
 
-    function name() public view returns (string memory) { return _name; }
-    function symbol() public view returns (string memory) { return _symbol; }
-    function decimals() public view returns (uint8) { return _decimals; }
-    function totalSupply() public view returns (uint256) { return _totalSupply; }
-    function balanceOf(address account) public view returns (uint256) { return _balances[account]; }
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
 
     function transfer(address to, uint256 value) public returns (bool) {
         _transfer(_msgSender(), to, value);
@@ -36,27 +53,37 @@ contract QuizToken is IERC20, Ownable {
     }
 
     function transferFrom(address from, address to, uint256 value) public returns (bool) {
-        _approve(from, _msgSender(), _allowances[from][_msgSender()] - value);
+        uint256 currentAllowance = _allowances[from][_msgSender()];
+        require(currentAllowance >= value, "ERC20: transfer amount exceeds allowance");
+        
+        _approve(from, _msgSender(), currentAllowance - value);
         _transfer(from, to, value);
         return true;
     }
 
-    // Allows the contract owner (Faucet) to mint new reward tokens
+    // Allows the contract owner (Faucet) to mint new reward tokens safely
     function mint(address to, uint256 amount) external onlyOwner {
+        require(_totalSupply + amount <= MAX_SUPPLY, "ERC20: Exceeds maximum supply cap");
         _totalSupply += amount;
         _balances[to] += amount;
+        emit Transfer(address(0), to, amount);
     }
 
     function _transfer(address from, address to, uint256 value) internal {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
+        require(_balances[from] >= value, "ERC20: transfer amount exceeds balance");
+
         _balances[from] -= value;
         _balances[to] += value;
+        emit Transfer(from, to, value);
     }
 
     function _approve(address owner, address spender, uint256 value) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
+
         _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
     }
 }
